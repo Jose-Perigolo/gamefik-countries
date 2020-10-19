110418008dbf4e379126403a2618c287cbd72404
JOSE PERIGOLO NETO <neto.perigolo@gmail.com>
2020-10-19 11:33:19 -0300

    Updates

---
+++ precache-manifest.0222598d650129d2cdec1c6f7b8214e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3dec7401fd1370d42a6feadfc5f56e4",
+    "url": "/gamefik-countries/%20/index.html"
+  },
+  {
+    "revision": "de1176519993ec41f547",
+    "url": "/gamefik-countries/%20/static/css/main.8983a3ac.chunk.css"
+  },
+  {
+    "revision": "0f9ab7dd63c9966d2eb6",
+    "url": "/gamefik-countries/%20/static/js/2.39355183.chunk.js"
+  },
+  {
+    "revision": "b3e2770e6336acd821638b70a2a55e3e",
+    "url": "/gamefik-countries/%20/static/js/2.39355183.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de1176519993ec41f547",
+    "url": "/gamefik-countries/%20/static/js/main.24fa5516.chunk.js"
+  },
+  {
+    "revision": "251cf68ce12d53006978",
+    "url": "/gamefik-countries/%20/static/js/runtime-main.e9a4f99b.js"
+  },
+  {
+    "revision": "5f18a76a1f6ab0c16dcfee65c62e18c1",
+    "url": "/gamefik-countries/%20/static/media/bg.5f18a76a.jpg"
+  }
+]);
